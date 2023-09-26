2eba757c5fe56bb408e0dc2add3dc8b0e01e1134
Andrei2028 <125688143+Andrei2028@users.noreply.github.com>
2023-09-26 08:23:12 +0300

    changed the user name in main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        User user1 = new User("Ion", "Deothon");
+        User user1 = new User("Ion", "Don");
         User user2 = new User("Miley", "Cyrus");
 
         System.out.println(user1.getInitials());
