a55b2b9cc0e733c1f774c901835b70ba8bda0d64
Andrei2028 <125688143+Andrei2028@users.noreply.github.com>
2023-12-11 11:16:15 +0200

    -

---
+++ src/main/java/OOPlab3/ArrayStack.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class ArrayStack<T> implements Stack<T> {
+    private ArrayList<T> elements;
+
+    public ArrayStack() {
+        elements = new ArrayList<>();
+    }
+
+    @Override
+    public void push(T element) {
+        elements.add(element);
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return elements.remove(elements.size() - 1);
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return elements.get(elements.size() - 1);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return elements.size();
+    }
+}
---
+++ src/main/java/OOPlab3/LinkedListStack.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+
+public class LinkedListStack<T> implements Stack<T> {
+    private LinkedList<T> elements;
+
+    public LinkedListStack() {
+        elements = new LinkedList<>();
+    }
+
+    @Override
+    public void push(T element) {
+        elements.addLast(element);
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return elements.removeLast();
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return elements.getLast();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return elements.size();
+    }
+}
